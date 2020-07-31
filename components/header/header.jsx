39a9b9dbf7398e93d03e01d4adf527d68b8367e6
ana-max <anastasiamaximovna17@mail.ru>
2020-07-31 18:04:20 +0500

    Добавление github ссылки в header

--- components/header/header.jsx
+++ components/header/header.jsx
@@ -11,6 +11,10 @@ export default function Header() {
                 <img className={styles.logo__image} src={`${createImagePath('circle.svg')}`} alt='Логотип' />
                 <p className={styles.logo__text}>STUDENTS</p>
             </Link>
+            <span className={styles.githubSpan}>by </span>
+            <a href='https://github.com/ana-max/students-react-app'
+               className={styles.githubLink}
+            >ana-max</a>
         </header>
     );
 }
